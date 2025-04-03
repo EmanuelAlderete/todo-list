0ae4f9a376022f5b1419a8fc17b645455e7656cf
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-03 18:44:22 -0300

    feat: criar usuário e token via API

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->string('password')),
+            ]);
+
+            event(new Registered($user));
+
+            Auth::login($user);
+
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+
+        } catch (ValidationException $e) {
+
+            return response()->json([
+                'message' => 'Erro de validação',
+                'errors' => $e->errors()
+            ], 422);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/user/register', [AuthController::class, 'register'])
+    ->middleware('guest')
+    ->name('register');
