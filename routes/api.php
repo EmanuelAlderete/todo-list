796e58faeac8e635260217f3e67b7040051ab525
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-03 18:57:24 -0300

    feat: adicionar método index no TaskListController

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/user/register', [AuthController::class, 'register'])
     ->middleware('guest')
     ->name('register');
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResources([
+        'task-list' => TaskListController::class,
+    ]);
+});
