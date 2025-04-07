45c5a431f0e678de6d0370447d0bedd5e8c3d300
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-04-07 09:35:23 -0300

    feat: adiciona priority no model Task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    protected $fillable = ['task_list_id', 'text', 'labels', 'status'];
+    protected $fillable = ['task_list_id', 'text', 'labels', 'status', 'priority'];
 
     protected $casts = [
         'labels' => 'array',
